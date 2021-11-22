89751196e68882a4c93d78f1fe656eac886d6586
徐蓁520370910043 <xuzhen1023@sjtu.edu.cn>
2021-11-23 00:15:44 +0800

    fix: more meaningful output

---
+++ src/ipa.cpp
@@ -0,0 +1,39 @@
+#ifdef __MaxOSX__ 
+#include <GLUT/glut.h>
+#else
+#include <GL/freeglut.h>
+#endif
+
+void display() {
+
+	/* clear window */
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	/* draw scene */
+	glutWireTeapot(.5);
+
+	/* flush drawing routines to the window */
+	glFlush();
+
+}
+
+int main(int argc, char *argv[]) {
+
+	/* initialize GLUT, using any commandline parameters passed to the program */
+	glutInit(&argc, argv);
+
+	/* setup the size, position, and display mode for new windows */
+	glutInitWindowSize(500, 500);
+	glutInitWindowPosition(0, 0);
+	glutInitDisplayMode(GLUT_RGB);
+
+	/* create and set up a window */
+	glutCreateWindow("Interstellar parking animation");
+	glutDisplayFunc(display);
+
+	/* tell GLUT to wait for events */
+	glutMainLoop();
+
+	return 0;
+
+}
