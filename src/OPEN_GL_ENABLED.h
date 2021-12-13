1efe7946b3a12fb140f0adc43e84fd9829a31820
Zhou Ziming <zimingzhou_03@sjtu.edu.cn>
2021-12-13 16:03:38 +0800

    create a neat dev branch for submission

--- src/OPEN_GL_ENABLED.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Ziming on 2021/12/9.
-//
-
-#ifndef P3_OPEN_GL_ENABLED_H
-#define P3_OPEN_GL_ENABLED_H
-#define OPEN_GL
-#endif //P3_OPEN_GL_ENABLED_H
--- src/Plot.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by Ziming on 2021/12/4.
-//
-
-#include "Plot.h"
